40e90f989476198a816ef2a4510302f7e62d4b25
SMORODYA74 <positivepel@gmail.com>
2025-05-04 20:15:35 +0500

    Подключение к БД Postgres

--- ChoreoCreatorBackend/ChoreoCreator.API/Program.cs
+++ ChoreoCreatorBackend/ChoreoCreator.API/Program.cs
@@ -1,11 +1,20 @@
+using ChoreoCreator.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ChoreoCreatorDbContext>(
+    options => 
+    {
+        options.UseNpgsql(builder.Configuration.GetConnectionString(nameof(ChoreoCreatorDbContext)));
+    });
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
 
 var app = builder.Build();
 
