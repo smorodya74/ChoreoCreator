86189f9a76f8593537eb000f74238de63e8169e0
Stepan Smorodnikov <60334241+smorodya74@users.noreply.github.com>
2025-05-13 12:32:17 +0500

    Добавлено выпадающее меню
    + Убрал модуль разработчика у Next.js

--- Frontend/choreocreator/app/components/EditorSidebar.tsx
+++ Frontend/choreocreator/app/components/EditorSidebar.tsx
@@ -1,9 +1,16 @@
 'use client';
 
-import React from 'react';
-import { PlusOutlined, DeleteOutlined } from '@ant-design/icons';
-import { Button, Layout, Typography } from 'antd';
+import React, { useState } from 'react';
+import {
+    PlusOutlined,
+    DeleteOutlined,
+    SettingOutlined,
+    UsergroupAddOutlined,
+    SaveOutlined
+} from '@ant-design/icons';
+import { Button, Layout, MenuProps, Typography } from 'antd';
 import { Dancer } from '../Models/Types';
+import Menu from 'antd/es/menu/menu';
 
 const { Sider } = Layout;
 const { Title } = Typography;
@@ -18,6 +25,53 @@ type EditorSidebarProps = {
     onSave: () => void;
 };
 
+type MenuItem = Required<MenuProps>["items"][number];
+
+const items: MenuItem[] = [
+    {
+        key: "1",
+        icon: <UsergroupAddOutlined />,
+        label: "Navigation One",
+        children: [
+            { key: "11", label: "Option 1" },
+            { key: "12", label: "Option 2" },
+            { key: "13", label: "Option 3" },
+        ],
+    },
+    {
+        key: "2",
+        icon: <SaveOutlined />,
+        label: "Navigation Two",
+        children: [
+            { key: "21", label: "Публикация" },
+            { key: "22", label: "Экспорт" },
+        ],
+    },
+];
+
+interface LevelKeysProps {
+    key?: string;
+    children?: LevelKeysProps[];
+}
+
+const getLevelKeys = (items1: LevelKeysProps[]) => {
+    const key: Record<string, number> = {};
+    const func = (items2: LevelKeysProps[], level = 1) => {
+        items2.forEach((item) => {
+            if (item.key) {
+                key[item.key] = level;
+            }
+            if (item.children) {
+                func(item.children, level + 1);
+            }
+        });
+    };
+    func(items1);
+    return key;
+};
+
+const levelKeys = getLevelKeys(items as LevelKeysProps[]);
+
 const EditorSidebar: React.FC<EditorSidebarProps> = ({
     dancerCount,
     dancers,
@@ -27,29 +81,70 @@ const EditorSidebar: React.FC<EditorSidebarProps> = ({
     onDeleteDancer,
     onSave,
 }) => {
+    const [stateOpenKeys, setStateOpenKeys] = useState(["1"]);
+
+    const onOpenChange: MenuProps["onOpenChange"] = (openKeys) => {
+        const currentOpenKey = openKeys.find(
+            (key) => stateOpenKeys.indexOf(key) === -1
+        );
+
+        /* Открытие меню */
+        if (currentOpenKey !== undefined) {
+            const repeatIndex = openKeys
+                .filter((key) => key !== currentOpenKey)
+                .findIndex((key) => levelKeys[key] === levelKeys[currentOpenKey]);
+
+            setStateOpenKeys(
+                openKeys
+
+                    /* Удаление ключа того же уровня */
+                    .filter((_, index) => index !== repeatIndex)
+
+                    /* Оставить только ключи текущего уровня и выше */
+                    .filter((key) => levelKeys[key] <= levelKeys[currentOpenKey])
+            );
+        } else {
+
+            /* Закрытие меню */
+            setStateOpenKeys(openKeys);
+        }
+    };
+
     return (
         <Sider
+            theme={'dark'}
             width={250}
             style={{
                 position: 'fixed',
                 top: 64,
                 bottom: 0,
                 left: 0,
-                background: '#041527',
+                // background: '#041527',
                 zIndex: 10,
             }}
         >
+            <Menu
+                theme={'dark'}
+                mode="inline"
+                defaultSelectedKeys={["1"]}
+                openKeys={stateOpenKeys}
+                onOpenChange={onOpenChange}
+                style={{ width: 256, zIndex:16 }}
+                items={items}
+                
+            />
             <div style={{ padding: 15 }}>
-                    <Title level={5} style={{ color: '#fff' }}>
-                        Танцоры: {dancerCount}
-                        <Button
-                            ghost
-                            style={{ marginLeft: 90 }} // Пересмотреть логику, сделать привязку к правому краю
-                            icon={<PlusOutlined />}
-                            onClick={onAddDancer}
-                        >
-                        </Button>
-                    </Title>
+                <Title level={5} style={{ color: '#fff' }}>
+                    Танцоры: {dancerCount}
+                    <Button
+                        ghost
+                        style={{ marginLeft: 90 }} // Пересмотреть логику, сделать привязку к правому краю
+                        icon={<PlusOutlined />}
+                        onClick={onAddDancer}
+                    >
+                    </Button>
+                </Title>
+                
             </div>
             {/* Список танцоров */}
             <div style={{ overflowY: 'auto', paddingBottom: 50 }}>
@@ -78,6 +173,7 @@ const EditorSidebar: React.FC<EditorSidebarProps> = ({
                         <span>{`Танцор ${index + 1}`}</span>
                     </div>
                 ))}
+                
             </div>
             {/* Кнопка удаления */}
             <div style={{ position: 'absolute', bottom: 40, width: '100%', padding: 8 }}>
