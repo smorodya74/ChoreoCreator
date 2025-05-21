53a6d73c1456f5d7964beeb06eeacb0c916dc069
Stepan Smorodnikov <60334241+smorodya74@users.noreply.github.com>
2025-05-21 22:51:12 +0500

    Обновлена модалка

--- Frontend/choreocreator/app/components/CreateUpdateScenario.tsx
+++ Frontend/choreocreator/app/components/CreateUpdateScenario.tsx
@@ -1,88 +1,74 @@
 import Modal from "antd/es/modal/Modal";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import TextArea from "antd/es/input/TextArea";
 import Input from "antd/es/input/Input";
-import { ScenarioRequest } from "../Models/Types";
 
-interface Props{
-    mode: Mode;
-    values: ScenarioRequest & { id?: string };
-    isModalOpen: boolean;
-    handleCancel: () => void;
-    handleCreate: (request: ScenarioRequest) => void;
-    handleUpdate: (id: string, request: ScenarioRequest) => void;
+interface ScenarioCreateRequest {
+    title: string;
+    description: string;
 }
 
-export enum Mode {
-    Save,
-    Publish,
+interface Props {
+    isModalOpen: boolean;
+    handleCancel: () => void;
+    handleCreate: (request: ScenarioCreateRequest) => void;
 }
 
 export const CreateUpdateScenario = ({
-    mode,
-    values,
     isModalOpen,
     handleCancel,
     handleCreate,
-    handleUpdate,
-} : Props) => {
+}: Props) => {
     const [title, setTitle] = useState<string>("");
     const [description, setDescription] = useState<string>("");
-    const [dancerCount, setDancerCount] = useState<number>(1);
 
-    useEffect(() => {
-        setTitle(values.title || "");
-        setDescription(values.description || "");
-        setDancerCount(values.dancerCount || 1);
-    }, [values]);
-
-    const handleOnOk = async () => {
-        const scenarioRequest: ScenarioRequest = {
+    const handleOnOk = () => {
+        const request: ScenarioCreateRequest = {
             title,
             description,
-            dancerCount,
-            formations: values.formations,
-            isPublished: mode === Mode.Publish,
         };
 
-        if (!values.id) {
-            handleCreate(scenarioRequest);
-        } else {
-            handleUpdate(values.id, scenarioRequest);
-        }
-    };    
+        handleCreate(request);
+        setTitle("");
+        setDescription("");
+    };
 
     return (
         <Modal
-            title={ mode === Mode.Save ? "Создать сценарий" : "Опубликовать сценарий" }
+            title="Создать сценарий"
             open={isModalOpen}
             onOk={handleOnOk}
-            onCancel={handleCancel}
-            cancelText={"Отменить"}
+            onCancel={() => {
+                handleCancel();
+                setTitle("");
+                setDescription("");
+            }}
+            cancelText="Отменить"
+            okText="Создать"
         >
             <div className="scenario_modal">
                 <div className="gradient-border-input">
                     <div className="gradient-border-input-inner">
-                    <Input
-                        value={title}
-                        onChange={(e) => setTitle(e.target.value)}
-                        placeholder="Название"
-                        variant="borderless"
-                    />
-                </div>
+                        <Input
+                            value={title}
+                            onChange={(e) => setTitle(e.target.value)}
+                            placeholder="Название"
+                            variant="borderless"
+                        />
+                    </div>
                 </div>
                 <div className="gradient-border-input">
                     <div className="gradient-border-input-inner">
-                    <TextArea
-                        value={description}
-                        onChange={(e) => setDescription(e.target.value)}
-                        autoSize={{ minRows: 3, maxRows: 3 }}
-                        placeholder="Описание"
-                        variant="borderless"
-                    />
-                </div>
+                        <TextArea
+                            value={description}
+                            onChange={(e) => setDescription(e.target.value)}
+                            autoSize={{ minRows: 3, maxRows: 3 }}
+                            placeholder="Описание"
+                            variant="borderless"
+                        />
+                    </div>
                 </div>
             </div>
         </Modal>
-    )
+    );
 };
