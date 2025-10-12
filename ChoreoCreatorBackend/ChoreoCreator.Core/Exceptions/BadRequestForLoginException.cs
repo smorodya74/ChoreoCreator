b2543dad2048fb8a67d0dd40b867aca9d3f213db
Stepan Smorodnikov <positivepel@gmail.com>
2025-10-12 22:15:55 +0500

    feat: Добавление пользовательских exceptions
    Убраны лишние using
    Переведены текущие exceptions

---
+++ ChoreoCreatorBackend/ChoreoCreator.Core/Exceptions/BadRequestForLoginException.cs
@@ -0,0 +1,13 @@
+﻿namespace ChoreoCreator.Core.Exceptions
+{
+    public class BadRequestForLoginException : Exception
+    {
+        /// <summary>
+        /// Исключение, которое выбрасывается, когда данные от учетной записи отсутствуют в системе
+        /// </summary>
+        public BadRequestForLoginException()
+            : base("Неверный логин/email или пароль")
+        { 
+        }
+    }
+}
